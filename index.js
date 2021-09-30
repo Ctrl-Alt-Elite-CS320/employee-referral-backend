82d767423e98fa24191da1bacf86b03bd310678e
pluto-de <70230259+pluto-de@users.noreply.github.com>
2021-09-30 01:43:35 -0400

    initial code to connect db to backend using async pool, and a little url extension to test functionality (usage: http://[server ip]/all   ------ > yields the result of SELECT * FROM employee query. Also added db.js file which sets up the pool connection. TODO: write functions for full CRUD capability as needed by backend (creates, reads, updates, deletes == INSERT, SELECT, UPDATE, DELETE queries)

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
+const pool = require("./db");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const hello = require("./routes/hello");
 app.use("/hello", hello);
+
+app.get("/all", async (req, res) => {
+	const results = await pool.query("select * from employee");
+	console.log(results);
+	res.send({"results": results});
+});
 
 const port = process.env.PORT || 4000;
 app.listen(port, function () {
