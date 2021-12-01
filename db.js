d7027529773555f3942fec6f0f2afeb1ef36f8d0
pluto-de <70230259+pluto-de@users.noreply.github.com>
2021-11-30 21:36:06 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
- 	"user":"cae",
- 	"password":"burgernuggetsnuggetsburger",
- 	"database":"caedb",
- 	"host":"localhost",
- 	"port":5432,
- 	"max":20,
- 	"connectionTimeoutMillis":0,
- 	"idleTimeoutMillis":0
+	"user":"cae",
+	"password":"burgernuggetsnuggetsburger",
+	"database":"caedb",
+	"host":"localhost",
+	"port":5432,
+	"max":20,
+	"connectionTimeoutMillis":0,
+	"idleTimeoutMillis":0
 });//TODO: determine ideal values for connectionTimeoutMillis and idleTimeoutMillis
 
 async function issueQuery(p, query) {
